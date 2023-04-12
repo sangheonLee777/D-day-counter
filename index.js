d9fb594c349b6117b7ce7f02025f011aba995864
sangheonLee777 <97956796+sangheonLee777@users.noreply.github.com>
2023-04-13 05:36:59 +0900

    ddd

--- index.js
+++ index.js
@@ -4,44 +4,38 @@ const month = document.querySelector(".target-month")
 const date = document.querySelector(".target-date")
 
 
-
-
 const dateFormMaker = function () {
   const yearValue = year.value;
   const monthValue = month.value;
   const dateValue = date.value;
-  const dateInput = yearValue + '-' + monthValue + '-' + dateValue
+  const dateInput = `${yearValue}-${monthValue}-${dateValue}`
   const nowDate = new Date ();
-  const dayDate = new Date (dateInput)
+  const dayDate = new Date (dateInput).setHours(0,0,0,0);
   const count = (dayDate - nowDate) / 1000 ;
-  const countDate = Math.floor(count / 3600 / 24);
+  
+  // count === 0 이면 d-day 끝
+  if (count === 0 || count < 0) {
+    console.log('입력 다시해라');
+  }else {
+    const countDate = Math.floor(count / 3600 / 24);
   const countHours = Math.floor(count / 3600) % 24 ;
   const countMinutes =  Math.floor(count / 60) % 60;
   const countSec = Math.floor(count) % 60;
   console.log(yearValue,monthValue,dateValue);
   console.log(countDate, countHours , countMinutes , countSec);
+  }
+
+  
+
+
+  // const countDate = Math.floor(count / 3600 / 24);
+  // const countHours = Math.floor(count / 3600) % 24 ;
+  // const countMinutes =  Math.floor(count / 60) % 60;
+  // const countSec = Math.floor(count) % 60;
+  // console.log(yearValue,monthValue,dateValue);
+  // console.log(countDate, countHours , countMinutes , countSec);
 }
 
 
 button.addEventListener("click",dateFormMaker)
 
-function getTime(){
-  const dDay = new Date('2023-4-15');
-  const today = new Date();
-  const difference = dDay.getTime() - today.getTime(),
-      // Ms 단위로 변환
-      secInMs = Math.floor(difference / 1000),
-      minInMs = Math.floor(secInMs / 60),
-      hourInMs = Math.floor(minInMs / 60),
-      days = Math.floor(hourInMs / 24),
-      // 남은 시간 계산
-      seconds = secInMs % 60,
-      minutes = minInMs % 60,
-      hours = minutes % 24;
-  // console.log(secInMs, minInMs , hourInMs , days);
-  console.log(days, hours ,minutes, seconds);
-  // console.log(minInMs, minutes);
-  // console.log(hours);
-}
-
-//getTime()
